a4016373404ba8c5d87b334f057fd82e62d428d0
Elmar Hinz <t3elmar@gmail.com>
2015-10-10 14:53:12 +0200

    Plot1 done. Add license.

--- common.R
+++ common.R
@@ -1,6 +1,17 @@
-##
+##############################################################################
+#                                                                            #
+#  Copyright (c) 2015 Elmar Hinz (github.com/elmar-hinz)                     #
+#                                                                            #
+#  Code project of the course                                                #
+#  Coursera Exploratory Data Analysis                                        #
+#                                                                            #
+#  License: MIT (see LICENSE.txt)                                            #
+#                                                                            #
+##############################################################################
+
+##############################################################################
 # Configuration
-##
+##############################################################################
 
 # Unix command to select lines from 1/2/2007 and 2/2/2007
 cmd <- "grep --regex=\"^Date\" --regex=\"^[12]/2/2007;\" data/household_power_consumption.txt > data/focus.txt"
@@ -8,18 +19,25 @@ cmd <- "grep --regex=\"^Date\" --regex=\"^[12]/2/2007;\" data/household_power_co
 # The file with the reduced data
 infile <- "data/focus.txt"
 
-readDf <- function() {
+##############################################################################
+# Get the data ready
+#
+# Does all steps to get a tidy dataframe for exploration.
+#
+# @return dataframe The data
+##############################################################################
+getData <- function() {
 
   # Reduce data by use of unix commands from 127M to 179K, roughly 1.4 per mille
   # You may run this on windows, as long as infile already exists.
   if(!file.exists(infile)) system(cmd)
-  
+
   # Read table
   df <- read.csv2(infile, stringsAsFactors = FALSE)
-  
-  # Adjust data types
+
+  # Handle numeric colums
   for(i in 3:9) { df[,i] = as.numeric(df[,i]); }
-  
+
   # Join Date and Time into DateTime
   df <- cbind(as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S"), df)
   names(df)[1] <- "DateTime"
--- plot1.R
+++ plot1.R
@@ -1,4 +1,49 @@
-# read dataframe
+##############################################################################
+#                                                                            #
+#  Copyright (c) 2015 Elmar Hinz (github.com/elmar-hinz)                     #
+#                                                                            #
+#  Code project of the course                                                #
+#  Coursera Exploratory Data Analysis                                        #
+#                                                                            #
+#  License: MIT (see LICENSE.txt)                                            #
+#                                                                            #
+##############################################################################
+
+##############################################################################
+# Red histogram of global active power (plot 1)
+##############################################################################
+
+########################################
+# Configure
+########################################
+
+## graphic
+title <- "Global Active Power"
+color <- "red"
+label.x <- "Global Active Power (kilowatts)"
+label.y <- "Frequency"
+values <- df$Global_active_power
+
+## file
+outfile <- "plot1.png"
+dev.function <- png
+width <- 480
+height <- 480
+
+########################################
+# Get the data
+########################################
 source("common.R")
-df <- readDf()
-hist(df$Global_active_power)
+df <- getData()
+
+########################################
+# Plot
+########################################
+hist(x = values, main = title, col = color, xlab = label.x, ylab = label.y)
+
+########################################
+# Save
+########################################
+dev.copy(dev.function, file = outfile, width = width, height = height)
+dev.off()
+
